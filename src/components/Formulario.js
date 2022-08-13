85f18bd7d4ef5e53e80eefbdcb87534b05b4fb37
nicolas andrade <nicolasandrade328@gmail.com>
2022-08-13 16:38:50 -0300

    eliminamos los console log

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -5,28 +5,23 @@ import ListaTareas from "./ListaTareas";
 
 const Formulario = () => {
 
-// crear un state
 let tareasLocalStorage = JSON.parse(localStorage.getItem('listaTareas')) || [];
 
 const [arregloTareas, setArregloTareas] = useState(tareasLocalStorage);
 const [tarea, setTarea] = useState('');
 
 useEffect(()=>{
-  console.log('prueba de ciclo de vida');
   localStorage.setItem('listaTareas',JSON.stringify(arregloTareas))
 },[arregloTareas]);
 
 const handleSubmit  = (e) => {
  e.preventDefault();
- console.log('prueba de submit');
- //actualizar lista de tareas
  setArregloTareas([...arregloTareas,tarea]);
  setTarea('');
 }
-//funcion para borrar tarea
+
 const borrarTarea = (nombre) =>{
  let arregloModificado = arregloTareas.filter((valor)=>{return valor !== nombre})
- // actualizar el state
  setArregloTareas(arregloModificado);
 }
   return (
